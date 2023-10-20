dd241e9732ae0fc3a5d92d7ff5688b738d987047
znooker <znooker_18@hotmail.com>
2023-10-20 11:40:55 +0200

    BasicAdminUI
    BasicAdminUI

---
+++ BVZ/Controllers/AdminAnimalController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BVZ.Controllers
+{
+    public class AdminAnimalController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
