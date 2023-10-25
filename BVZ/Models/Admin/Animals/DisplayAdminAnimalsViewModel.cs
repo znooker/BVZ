7c4edf775e796cd733c4ac72c873df480982ee37
ToBeOtt <119058608+ToBeOtt@users.noreply.github.com>
2023-10-25 19:09:47 +0200

    minor fixes

--- BVZ/Models/Admin/Animals/DisplayAdminAnimalsViewModel.cs
+++ BVZ/Models/Admin/Animals/DisplayAdminAnimalsViewModel.cs
@@ -4,7 +4,8 @@ namespace BVZ.Models.Admin.Animals
 {
     public class DisplayAdminAnimalsViewModel
     {
-        public List<Animal> Animals { get; set; }
+        public List<Animal>? Animals { get; set; }
+        public List<string>? AnimalTypes { get; set; }
 
         public string? Message { get; set; }
         public string? Status { get; set; }
